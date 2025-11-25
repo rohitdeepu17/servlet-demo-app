8712ca97356e09ab5b493a1271b383399d875530
Singla Software <rohitdeepu17@gmail.com>
2025-11-25 08:18:24 +0530

    Add SqServlet and forward result from AddServlet
    Introduced SqServlet to compute the square of a value set by AddServlet. Modified AddServlet to forward the sum result to SqServlet using a request attribute. Updated web.xml to register and map the new servlet.

--- src/main/java/com/codesminds/AddServlet.java
+++ src/main/java/com/codesminds/AddServlet.java
@@ -3,19 +3,23 @@ package com.codesminds;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i+j;
 		
-		PrintWriter out = res.getWriter();
-		out.println("result is "+k);
+		req.setAttribute("k", k);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
 	}
 
 }
---
+++ src/main/java/com/codesminds/SqServlet.java
@@ -0,0 +1,24 @@
+package com.codesminds;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet{
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int k = (int)req.getAttribute("k");
+		
+		int square = k*k;
+		
+		PrintWriter out = res.getWriter();
+		out.println("result is "+square);
+		
+	}
+
+}
